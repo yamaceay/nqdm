ec0d5962177b6ccf172edcb360cbb11c211d16fe
yamaceay <yamacerenay2001@gmail.com>
2021-05-14 01:39:17 +0200

    alternative repo

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name = 'nqdm',
-    packages = ['nqdm'],
-    version = '0.1.0',  # Ideally should be same as your GitHub release tag varsion
-    description = 'NQDM -- An extension of TQDM which enables you to loop over multiple objects simultaneously, and specify the depth of iteration for each object. It is just pure Python magic, no extra libraries needed. It is customizable, minimal and open source.',
-    author = 'Yamac Eren Ay',
-    author_email = 'yamacerenay2001@gmail.com',
-    url = 'https://github.com/yamaceay/nqdm',
-    download_url = "https://github.com/yamaceay/nqdm/archive/refs/tags/v0.1.0.tar.gz",
-    keywords = ['nqdm', 'NQDM', "progress bar", "tqdm"],
-    classifiers = [],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nqdm",
+    version="0.1.0",
+    author="Yamac Eren Ay",
+    author_email="yamacerenay2001@gmail.com",
+    description="NQDM -- An extension of TQDM which enables you to loop over multiple objects simultaneously, and specify the depth of iteration for each object. It is just pure Python magic, no extra libraries needed. It is customizable, minimal and open source.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yamaceay/nqdm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6'
 )
