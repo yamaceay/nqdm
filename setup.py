5bee8abf8c6459a1dcda1b047c6b2d714887e896
yamac_eren_ay_1 <yamac.ay@campus.tu-berlin.de>
2022-02-25 22:31:59 +0100

    new update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nqdm",
-    version="0.1.8",
+    version="0.1.9",
     author="Yamac Eren Ay",
     author_email="yamacerenay2001@gmail.com",
     description="NQDM -- An extension of TQDM which enables you to loop over multiple objects simultaneously, and specify the depth of iteration for each object. It is just pure Python magic, no extra libraries needed. It is customizable, minimal and open source.",
@@ -18,5 +18,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
+    install_requires=[
+          'tqdm==4.6.0'
+      ],
     python_requires='>=3.6'
 )
